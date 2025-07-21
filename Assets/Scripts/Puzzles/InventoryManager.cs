74a396466819a1884c4aec6028de2946a8f08eb6
Adriaan <kooistraadriaan@gmail.com>
2025-07-21 11:19:44 +0200

    Feat: Add puzzle scene and logbook

---
+++ Assets/Scripts/Puzzles/InventoryManager.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject logBook;
+    private bool _logbookOpen = false;
+    private InputActions _inputActions;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _inputActions.Inventory.ToggleLogbook.performed += ctx => ToggleLogbook();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        _inputActions = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
+    }
+
+    private void ToggleLogbook()
+    {
+        Debug.Log("Triggered");
+        _logbookOpen = !_logbookOpen;
+        logBook.SetActive(_logbookOpen);
+    }
+}
