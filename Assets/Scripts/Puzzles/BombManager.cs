23257bfab4315deb5fdda3570db12e17a7cce774
Adriaan <kooistraadriaan@gmail.com>
2025-07-24 13:43:45 +0200

    Fix: removed small redundant lines of code

--- Assets/Scripts/Puzzles/BombManager.cs
+++ Assets/Scripts/Puzzles/BombManager.cs
@@ -47,22 +47,6 @@ public class BombManager : MonoBehaviour
     {
         _inputActions.Disable();
     }
-    
-    // void OnMouseDown()
-    // {
-    //     Debug.Log("Triggered");
-    //     screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-    //     
-    //     offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-    // }
-    //
-    // void OnMouseDrag()
-    // {
-    //     Vector3 cursorScreenPoint = new  Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-    //     
-    //     Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorScreenPoint) + offset;
-    //     transform.position = cursorPosition;
-    // }
 
     void GetPuzzleScripts()
     {
