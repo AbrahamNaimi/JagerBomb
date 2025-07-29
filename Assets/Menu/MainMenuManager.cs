ee3b87cb2e121751d364ec7311259419c96ed178
Emil Ebrahimi <s1132776@student.hsleiden.nl>
2025-07-29 14:23:31 +0200

    Added game menu with pause
    Added the game menu and pause menu. Its awesome really

--- Assets/Menu/MainMenuManager.cs
+++ Assets/Menu/MainMenuManager.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
+
 public class MainMenuManager : MonoBehaviour
 {
-  public void LoadScene(string sceneName)
+    public void LoadScene(string sceneName)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(sceneName);
     }
 
