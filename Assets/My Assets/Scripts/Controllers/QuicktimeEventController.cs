5ca3c96bdd8cf50c14d6e2aa2b1f276fbbde0f50
Adriaan <kooistraadriaan@gmail.com>
2025-12-12 12:47:17 +0100

    Feat: QTE is now a ring where user must click at the right time. Afterwards a success or fail message is displayed.

--- Assets/My Assets/Scripts/Controllers/QuicktimeEventController.cs
+++ Assets/My Assets/Scripts/Controllers/QuicktimeEventController.cs
@@ -12,18 +12,53 @@ namespace My_Assets.Controllers
     public class QuicktimeEventController : MonoBehaviour
     {
         public SitSpot sitSpot;
-        
+
+        public GameObject mouseClickImage;
         public GameObject zoneRotator;
         public GameObject bottleRotator;
+        public TextMeshProUGUI messageTMP;
 
-        private float rotationSpeed = 180.0f;
+        public float baseSpeed = 60.0f;
+
+        private float _rotationSpeed;
         private float _currentBottleRotation;
 
         private int _bottleStartRotation;
         private int _zoneRotation;
 
-        public void Start()
+        private bool _isFinished;
+        private bool _isSuccessful;
+
+        private string _successMessage = "<color=#008000>Well done!</color> \nYou're handling it well my friend.";
+        private string _failMessage = "<color=#FF0000>Oof!</color> \nIt's tough isn't it.";
+
+        private void Update()
         {
+            if (!_isFinished)
+            {
+                float rotation = _rotationSpeed * -1 * Time.deltaTime;
+                bottleRotator.transform.Rotate(0, 0, rotation, Space.Self);
+                _currentBottleRotation += rotation;
+            
+            
+                if (Mouse.current.leftButton.wasPressedThisFrame)
+                {
+                    _isSuccessful = _currentBottleRotation % 360 > _zoneRotation - 30 &&
+                                         _currentBottleRotation % 360 < _zoneRotation + 30;
+
+                    _isFinished = true;
+
+                    StartCoroutine(ShowSuccess());
+                }
+            }
+        }
+
+        public void Init()
+        {
+            _isFinished = false;
+            _isSuccessful = false;
+            messageTMP.text = "";
+            mouseClickImage.SetActive(true);
             _bottleStartRotation = Random.Range(0, 359);
             _zoneRotation = Random.Range(_bottleStartRotation + 90, _bottleStartRotation + 320) % 360;
             _bottleStartRotation *= -1;
@@ -33,115 +68,23 @@ namespace My_Assets.Controllers
             bottleRotator.transform.Rotate(0, 0, _bottleStartRotation);
             zoneRotator.transform.Rotate(0, 0, _zoneRotation);
             
-            rotationSpeed = 60.0f * PlayerPrefs.GetInt("Level") * (PlayerPrefs.GetInt("Drunkness") + 1);
+            _rotationSpeed = baseSpeed * PlayerPrefs.GetInt("Level") * (PlayerPrefs.GetInt("Drunkness") + 1);
+            gameObject.SetActive(true);
         }
 
-        private void Update()
+        private IEnumerator ShowSuccess()
         {
-            float rotation = rotationSpeed * -1 * Time.deltaTime;
-            bottleRotator.transform.Rotate(0, 0, rotation, Space.Self);
-            _currentBottleRotation += rotation;
-
-            if (Mouse.current.leftButton.wasPressedThisFrame)
-            {
-                bool success = _currentBottleRotation % 360 > _zoneRotation - 30 &&
-                               _currentBottleRotation % 360 < _zoneRotation + 30;
-                sitSpot.DrinkingQTEFinished(success);
-                Destroy(gameObject);
-            }
+            mouseClickImage.SetActive(false);
+            
+            messageTMP.text = _isSuccessful ? _successMessage : _failMessage;
+            
+            yield return new WaitForSeconds(1.5f);
+            
+            bottleRotator.transform.localEulerAngles = Vector3.zero;
+            zoneRotator.transform.localEulerAngles = Vector3.zero;
+            
+            sitSpot.DrinkingQTEResult(_isSuccessful);
+            gameObject.SetActive(false);
         }
-
-        // public GameObject QteObject;
-        // public SitSpot sitSpot;
-        // public TextMeshProUGUI DisplayBox;
-        // public TextMeshProUGUI PassBox;
-        //
-        // public int QTEGen;
-        // public int WaitingForKey;
-        // public bool CorrectKeyPressed;
-        // public bool CountingDown;
-        //
-        //
-        // public void Update()
-        // {
-        //     if (WaitingForKey == 0)
-        //     {
-        //         QTEGen = Random.Range(0, 3);
-        //         CountingDown = true;
-        //         StartCoroutine(CountDown());
-        //
-        //         switch (QTEGen)
-        //         {
-        //             case 0:
-        //                 WaitingForKey = 1;
-        //                 DisplayBox.text = "Q";
-        //                 break;
-        //
-        //             case 1:
-        //                 WaitingForKey = 1;
-        //                 DisplayBox.text = "R";
-        //                 break;
-        //
-        //             case 2:
-        //                 WaitingForKey = 1;
-        //                 DisplayBox.text = "T";
-        //                 break;
-        //         }
-        //     }
-        //
-        //
-        //     if (WaitingForKey == 1)
-        //     {
-        //         if (Keyboard.current.anyKey.wasPressedThisFrame)
-        //         {
-        //             switch (QTEGen)
-        //             {
-        //                 case 0:
-        //                     CorrectKeyPressed = Keyboard.current.qKey.wasPressedThisFrame;
-        //                     break;
-        //
-        //                 case 1:
-        //                     CorrectKeyPressed = Keyboard.current.rKey.wasPressedThisFrame;
-        //                     break;
-        //
-        //                 case 2:
-        //                     CorrectKeyPressed = Keyboard.current.tKey.wasPressedThisFrame;
-        //                     break;
-        //             }
-        //             StartCoroutine(KeyPressing());
-        //             WaitingForKey = -1;
-        //         }
-        //     }
-        // }
-        //
-        // IEnumerator KeyPressing()
-        // {
-        //     QTEGen = 10;
-        //     if (!CorrectKeyPressed)
-        //     {
-        //         StartCoroutine(sitSpot.SitAndDrink());
-        //         PassBox.text = "Incorrect. Man that bums me out, I can't do anything. Time for a drink.";
-        //     }
-        //     else
-        //     {
-        //         PassBox.text = "I gotta lay off the booze man";
-        //     }
-        //     CountingDown = false;
-        //     yield return new WaitForSeconds(1.5f);
-        //     Destroy(QteObject);
-        // }
-        //
-        // IEnumerator CountDown()
-        // {
-        //     yield return new WaitForSeconds(1.5f);
-        //     if (CountingDown)
-        //     {
-        //         StartCoroutine(sitSpot.SitAndDrink());
-        //         PassBox.text = "Too late. Man that bums me out, I can't do anything right. Time for a drink.";
-        //     }
-        //
-        //     yield return new WaitForSeconds(1.5f);
-        //     Destroy(QteObject);
-        // }
     }
 }
